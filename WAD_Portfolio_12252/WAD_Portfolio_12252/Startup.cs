d517bc6f43d6ab36b23d1f4b226e127eadb63792
Anastasiya <91121654+00012252@users.noreply.github.com>
2023-03-17 17:32:15 +0500

    change: appsettings, startup

--- WAD_Portfolio_12252/WAD_Portfolio_12252/Startup.cs
+++ WAD_Portfolio_12252/WAD_Portfolio_12252/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WAD_Portfolio_12252.MovieDbContext;
+using WAD_Portfolio_12252.Repository;
 
 namespace WAD_Portfolio_12252
 {
@@ -24,6 +27,9 @@ namespace WAD_Portfolio_12252
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("API")));
+            services.AddTransient<IMovieRepository, MovieRepository>();
+            services.AddTransient<IGenreRepository, GenreRepository>();
             services.AddControllers();
         }
 
