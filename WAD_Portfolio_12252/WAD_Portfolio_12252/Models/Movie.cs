fde1293d179b7d6f14bd1b9c62751e55f71a2d68
Anastasiya <91121654+00012252@users.noreply.github.com>
2023-03-17 17:25:35 +0500

    created: models

---
+++ WAD_Portfolio_12252/WAD_Portfolio_12252/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace WAD_Portfolio_12252.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
