16326a33c8fa33ec43414814827c5e77050d25b9
Anastasiya <91121654+00012252@users.noreply.github.com>
2023-03-17 17:31:02 +0500

    created: controllers

---
+++ WAD_Portfolio_12252/WAD_Portfolio_12252/Controllers/GenreController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
+using WAD_Portfolio_12252.Models;
+using WAD_Portfolio_12252.Repository;
+
+namespace WAD_Portfolio_12252.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly IGenreRepository _genreRepository;
+        public GenreController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        // GET: api/<GenreController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var genre = _genreRepository.GetAllGenres();
+            return new OkObjectResult(genre);
+        }
+
+        // GET api/<GenreController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var genre = _genreRepository.GetGenreById(id);
+            return new OkObjectResult(genre);
+        }
+
+        // POST api/<GenreController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Genre genre)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _genreRepository.InsertGenre(genre);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = genre.Id }, genre);
+            }
+        }
+
+        // PUT api/<GenreController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Genre genre)
+        {
+            if (genre != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _genreRepository.UpdateGenre(genre);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE api/<GenreController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _genreRepository.DeleteGenre(id);
+            return new OkResult();
+        }
+    }
+}
---
+++ WAD_Portfolio_12252/WAD_Portfolio_12252/Controllers/MovieController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
+using WAD_Portfolio_12252.Models;
+using WAD_Portfolio_12252.Repository;
+
+namespace WAD_Portfolio_12252.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+
+        // GET: api/<MovieController>
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var movies = _movieRepository.GetAllMovies();
+            return new OkObjectResult(movies);
+        }
+
+        // GET api/<MovieController>/5
+        [HttpGet, Route("{id}", Name = "GetM")]
+        public IActionResult Get(int id)
+        {
+            var movie = _movieRepository.GetMovieById(id);
+            return new OkObjectResult(movie);
+        }
+
+        // POST api/<MovieController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _movieRepository.InsertMovie(movie);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+            }
+        }
+
+        // PUT api/<MovieController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Movie movie)
+        {
+            if (movie != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _movieRepository.UpdateMovie(movie);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE api/<MovieController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _movieRepository.DeleteMovie(id);
+            return new OkResult();
+        }
+    }
+}
