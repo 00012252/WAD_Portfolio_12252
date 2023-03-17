3632fce8c0091a4416bba6b6dce5886ec8876a01
Anastasiya <91121654+00012252@users.noreply.github.com>
2023-03-17 17:27:07 +0500

    created: dbcontext

---
+++ WAD_Portfolio_12252/WAD_Portfolio_12252/MovieDbContext/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WAD_Portfolio_12252.Models;
+
+namespace WAD_Portfolio_12252.MovieDbContext
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
